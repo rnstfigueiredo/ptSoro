36823f86b4c7ca975efcac7dbad5bf51869aa255
Ernesto Figueiredo <ernesto.figueiredo@outlook.pt>
2024-04-17 13:31:18 -0300

    Delete soro directory

--- soro/inst/app/www/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-document.getElementsByClassName("leaflet-control-zoom-in").innerHTML = 'text';
-document.getElementsByClassName("leaflet-control-zoom-in").setAttribute('title', 'after click');
-document.getElementById('link').setAttribute('title', 'after click');
-});
-
-
-
