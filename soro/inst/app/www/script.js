d177c671d03e89386d1bea4febe44796126e6178
Ernesto Figueiredo <ernesto.figueiredo@outlook.pt>
2024-04-17 13:34:39 -0300

    Add files via upload

---
+++ soro/inst/app/www/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+document.getElementsByClassName("leaflet-control-zoom-in").innerHTML = 'text';
+document.getElementsByClassName("leaflet-control-zoom-in").setAttribute('title', 'after click');
+document.getElementById('link').setAttribute('title', 'after click');
+});
+
+
+
